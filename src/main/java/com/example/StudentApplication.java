eaf6ad2bd8faeb46728a8a706bed7b9489816154
manasvrm227 <manasvrm227@gmail.com>
2021-06-11 07:07:53 +0530

    removed deprecated methods

--- src/main/java/com/example/StudentApplication.java
+++ src/main/java/com/example/StudentApplication.java
@@ -1,17 +1,16 @@
 package com.example;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.example.bean.Student;  
 
 public class StudentApplication {
 	public static void main(String[] args) {
-		Resource resource=new ClassPathResource("applicationContext.xml");  
-	    BeanFactory factory=new XmlBeanFactory(resource);  
-	      
-	    Student student=(Student)factory.getBean("studentbean");  
+	    ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");  
+
+	    Student student = (Student) ctx.getBean("studentbean");
+	    ((ClassPathXmlApplicationContext) ctx).close();
+	    
 	    student.displayName();
 	}
 }
